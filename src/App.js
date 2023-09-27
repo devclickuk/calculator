448c074f5a240ec0f11e6cda8308ee7f54032ce7
Dave Amison <dave.amison@sixstar.global>
2023-09-27 21:56:19 +0000

    more styling and var colors

--- src/App.js
+++ src/App.js
@@ -8,30 +8,30 @@ function App() {
         <span className='display'>0</span>
       </div>
       <div className='numbersContainer'>
-        <div className='calcButton'>%</div>
-        <div className='calcButton'>CE</div>
-        <div className='calcButton'>C</div>
-        <div className='calcButton'>&lt;-</div>
-        <div className='calcButton'>1/x</div>
-        <div className='calcButton'>x<sup>2</sup></div>
-        <div className='calcButton'>√</div>
-        <div className='calcButton'>/</div>
+        <div className='calcButton altButton'>%</div>
+        <div className='calcButton altButton'>CE</div>
+        <div className='calcButton altButton'>C</div>
+        <div className='calcButton altButton'>&lt;-</div>
+        <div className='calcButton altButton'>1/x</div>
+        <div className='calcButton altButton'>x<sup>2</sup></div>
+        <div className='calcButton altButton'>√</div>
+        <div className='calcButton altButton'>/</div>
         <div className='calcButton'>7</div>
         <div className='calcButton'>8</div>
         <div className='calcButton'>9</div>
-        <div className='calcButton'>*</div>
+        <div className='calcButton altButton'>*</div>
         <div className='calcButton'>4</div>
         <div className='calcButton'>5</div>
         <div className='calcButton'>6</div>
-        <div className='calcButton'>-</div>
+        <div className='calcButton altButton'>-</div>
         <div className='calcButton'>1</div>
         <div className='calcButton'>2</div>
         <div className='calcButton'>3</div>
-        <div className='calcButton'>+</div>
+        <div className='calcButton altButton'>+</div>
         <div className='calcButton'>+/-</div>
         <div className='calcButton'>0</div>
         <div className='calcButton'>.</div>
-        <div className='calcButton'>=</div>
+        <div className='calcButton  equalsButton'>=</div>
 
       </div>
     </div>
