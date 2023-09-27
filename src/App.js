861539104416fd83eb7e00b0c4220d3bd49d2f35
Dave Amison <dave.amison@sixstar.global>
2023-09-27 21:45:25 +0000

    basic layout and basic css

--- src/App.js
+++ src/App.js
@@ -2,8 +2,38 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Page
+    <div className="container">
+      <div className='header'>
+        <span className='history'>0</span>
+        <span className='display'>0</span>
+      </div>
+      <div className='numbersContainer'>
+        <div className='calcButton'>%</div>
+        <div className='calcButton'>CE</div>
+        <div className='calcButton'>C</div>
+        <div className='calcButton'>&lt;-</div>
+        <div className='calcButton'>1/x</div>
+        <div className='calcButton'>x<sup>2</sup></div>
+        <div className='calcButton'>√</div>
+        <div className='calcButton'>/</div>
+        <div className='calcButton'>7</div>
+        <div className='calcButton'>8</div>
+        <div className='calcButton'>9</div>
+        <div className='calcButton'>*</div>
+        <div className='calcButton'>4</div>
+        <div className='calcButton'>5</div>
+        <div className='calcButton'>6</div>
+        <div className='calcButton'>-</div>
+        <div className='calcButton'>1</div>
+        <div className='calcButton'>2</div>
+        <div className='calcButton'>3</div>
+        <div className='calcButton'>+</div>
+        <div className='calcButton'>+/-</div>
+        <div className='calcButton'>0</div>
+        <div className='calcButton'>.</div>
+        <div className='calcButton'>=</div>
+
+      </div>
     </div>
   );
 }
