79bdcb17b4830e779e54cbef88abfd39aa3fb56d
Dave Amison <dave.amison@sixstar.global>
2023-09-28 07:24:39 +0000

    numbers when pressed enter on display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [display, setDisplay] = useState(0);
+
+  const numberPressed = (e) => {
+    if (display === 0) {
+      setDisplay(e.target.innerText);
+      return;
+    }
+    if (display.includes('.') && e.target.innerText === '.') return;
+    setDisplay(display + e.target.innerText);
+  }
+
   return (
     <div className="container">
       <div className='header'>
         <span className='history'>0</span>
-        <span className='display'>0</span>
+        <span className='display'>{display}</span>
       </div>
       <div className='numbersContainer'>
         <div className='calcButton altButton'>%</div>
@@ -16,21 +28,21 @@ function App() {
         <div className='calcButton altButton'>x<sup>2</sup></div>
         <div className='calcButton altButton'>√</div>
         <div className='calcButton altButton'>/</div>
-        <div className='calcButton'>7</div>
-        <div className='calcButton'>8</div>
-        <div className='calcButton'>9</div>
+        <div className='calcButton' onClick={(e) => numberPressed(e)}>7</div>
+        <div className='calcButton' onClick={(e) => numberPressed(e)}>8</div>
+        <div className='calcButton' onClick={(e) => numberPressed(e)}>9</div>
         <div className='calcButton altButton'>*</div>
-        <div className='calcButton'>4</div>
-        <div className='calcButton'>5</div>
-        <div className='calcButton'>6</div>
+        <div className='calcButton' onClick={(e) => numberPressed(e)}>4</div>
+        <div className='calcButton' onClick={(e) => numberPressed(e)}>5</div>
+        <div className='calcButton' onClick={(e) => numberPressed(e)}>6</div>
         <div className='calcButton altButton'>-</div>
-        <div className='calcButton'>1</div>
-        <div className='calcButton'>2</div>
-        <div className='calcButton'>3</div>
+        <div className='calcButton' onClick={(e) => numberPressed(e)}>1</div>
+        <div className='calcButton' onClick={(e) => numberPressed(e)}>2</div>
+        <div className='calcButton' onClick={(e) => numberPressed(e)}>3</div>
         <div className='calcButton altButton'>+</div>
         <div className='calcButton'>+/-</div>
-        <div className='calcButton'>0</div>
-        <div className='calcButton'>.</div>
+        <div className='calcButton' onClick={(e) => numberPressed(e)}>0</div>
+        <div className='calcButton' onClick={(e) => numberPressed(e)}>.</div>
         <div className='calcButton  equalsButton'>=</div>
 
       </div>
